54ed24ac60f90fa910936637255be70c2d053417
BeatrizMoran <beatriz.moran@ikasle.egibide.org>
2023-05-23 12:49:24 +0200

    pruebas temporada

--- Programacion/kings_league/src/test/java/com/boajp/AplicacionTest.java
+++ Programacion/kings_league/src/test/java/com/boajp/AplicacionTest.java
@@ -1,6 +1,10 @@
 package com.boajp;
 
+import com.boajp.modelo.TemporadaEntidad;
+import com.boajp.servicios.TemporadasServicio;
 import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -8,5 +12,18 @@ class AplicacionTest {
 
     @Test
     void getVentanaControlador() {
+    }
+
+    @Test
+    public void insertarTemporada() throws Exception {
+        TemporadasServicio temporadasServicio = new TemporadasServicio();
+        temporadasServicio.anadirTemporada((short) 2023, LocalDate.of(2023,06,12),LocalDate.of(2023,06,23));
+    }
+
+    @Test
+    public void modificarTemporada()throws Exception{
+        TemporadasServicio temporadasServicio = new TemporadasServicio();
+        TemporadaEntidad temporadaEntidad = new TemporadaEntidad((short) 2023, LocalDate.of(2023,06,12),LocalDate.of(2023,06,23));
+        temporadasServicio.modificarTemporada(temporadaEntidad);
     }
 }
